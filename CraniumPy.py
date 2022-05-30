e981e351f81e6e595f7408883317e693efb12348
T-AbdelAlim <abdelalim.tareq@gmail.com>
2022-05-30 14:19:47 +0200

    updated executable v0.2.2

--- CraniumPy.py
+++ CraniumPy.py
@@ -168,7 +168,7 @@ class MainWindow(Qt.QMainWindow, GuiMethods):
 
 
 if __name__ == '__main__':
-    print('Running CraniumPy 0.2.1')
+    print('Running CraniumPy 0.2.2')
     root = Tk()
     root.withdraw()  #removes tkwindow from file import
     app = Qt.QApplication(sys.argv)
